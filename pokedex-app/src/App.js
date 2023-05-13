ab03f5194aee5343594ecf9d056e8d64f7560f89
Itziar Uzqueda <itziar.uzqueda@movalsys.com>
2023-05-13 14:33:52 +0200

    diseño card finalizado

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -7,10 +6,50 @@ function App() {
 
 const Card = () => {
   return (
-    <div class="card-container">
-      <div class="card-title">
-        <p class="pokemon-name">Bulbasaur</p>
-        <p class="pokemon-number">#001</p>
+    <div className="card-container">
+      <div className="card-title">
+        <p className="pokemon-name">Bulbasaur</p>
+        <p className="pokemon-number">#001</p>
+      </div>
+      <img
+        className="pokemon-img"
+        src="_assets/pokemons/bulbasaur.png"
+        alt="bulbasaur"
+      />
+      <div className="card-info">
+        <div className="pokemon-type-container">
+          <p className="pokemon-type">Grass</p>
+          <p className="pokemon-type">Poisson</p>
+        </div>
+        <p className="about">About</p>
+        <div className="pokemon-features">
+          <div className="pokemon-weight">
+            <div className="card-measure">
+              <img
+                className="feature-icon"
+                src="_assets/icons/weight.png"
+                alt="weight"
+              />
+              <p>6.9 kg</p>
+            </div>
+            <p className="feature-description-tag">Weight</p>
+          </div>
+          <div className="pokemon-height">
+            <div className="card-measure">
+              <img
+                className="feature-icon"
+                src="_assets/icons/ruler.png"
+                alt="ruler"
+              />
+              <p>0.7m</p>
+            </div>
+            <p className="feature-description-tag">Height</p>
+          </div>
+        </div>
+        <p className="text-info">
+          There is a plant seed on its back right from the day this Pokémon is
+          born
+        </p>
       </div>
     </div>
   );
