c2742f744beef319176bd06b600deff7bca81ff4
Itziar Uzqueda <itziar.uzqueda@movalsys.com>
2023-05-15 10:18:45 +0200

    Cambio de naming e imagen de pokemon colocada

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -8,47 +8,47 @@ const Card = () => {
   return (
     <div className="card__container">
       <section className="card__title">
-        <p className="pokemon__name">Bulbasaur</p>
-        <p className="pokemon__number">#001</p>
+        <p className="card__pokemon-name">Bulbasaur</p>
+        <p className="card__pokemon-number">#001</p>
       </section>
       <section className="card__info">
-        <div className="img__container">
+        <div className="card__img__container">
           <img
-            className="pokemon__img"
-            src="_assets/pokemons/mew.png"
+            className="card__pokemon__img"
+            src="_assets/pokemons/bulbasaur.png"
             alt="bulbasaur"
           />
         </div>
-        <div className="pokemon__type__container">
-          <p className="pokemon__type">Grass</p>
-          <p className="pokemon__type">Poisson</p>
+        <div className="card__pokemon__type__container">
+          <p className="card__pokemon__type">Grass</p>
+          <p className="card__pokemon__type">Poisson</p>
         </div>
         <p className="about">About</p>
-        <div className="pokemon__features">
-          <div className="pokemon__weight">
-            <div className="card__measure">
+        <div className="card__pokemon__features__container">
+          <div className="card__pokemon__feature-weight">
+            <div className="card__feature__weight_container">
               <img
-                className="feature__icon"
+                className="card__feature__icon-weight"
                 src="_assets/icons/weight.png"
                 alt="weight"
               />
               <p>6.9 kg</p>
             </div>
-            <p className="feature__description__tag">Weight</p>
+            <p className="card__feature__description__tag">Weight</p>
           </div>
-          <div className="pokemon__height">
-            <div className="card__measure">
+          <div className="card__pokemon__feature-height">
+            <div className="card__feature__height_container">
               <img
-                className="feature__icon"
+                className="card_feature__icon"
                 src="_assets/icons/ruler.png"
                 alt="ruler"
               />
               <p>0.7m</p>
             </div>
-            <p className="feature__description__tag">Height</p>
+            <p className="card__feature__description__tag">Height</p>
           </div>
         </div>
-        <p className="text__info">
+        <p className="card__text__info">
           There is a plant seed on its back right from the day this Pokémon is
           born
         </p>
